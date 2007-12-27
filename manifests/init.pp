bdf9527f62e519eb747544e6235eb61d77395ab9
mh <mh@immerda.ch>
2007-12-27 17:09:26 +0000

    added mc module

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# modules/mc/manifests/init.pp - manage mc stuff
+# Copyright (C) 2007 admin@immerda.ch
+#
+
+modules_dir { "mc": }
+
+class mc {
+i	package{mc:
+                name => mc,
+                alias   => 'mc',
+                category => $operatingsystem ? {
+                        gentoo => 'app-misc',
+                        default => '',
+                },
+                ensure => present,
+        }
+}
